4e86c923c6c96937aa7827a05462b9e98b38c919
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-05-21 19:26:31 +0200

    refactored metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,35 +1,13 @@
-from typing import Union, Tuple, Optional
-
 import torch
 
 
 class AverageMeter:
-    """
-    Computes and stores the average of a given value(s). The average value is store in the `avg` parameter.
-
-    Examples:
-            ```python
-            # Initialize a meter to record loss
-            losses = AverageMeter()
-            # Update meter after every minibatch update
-            losses.update(loss_value, batch_size)
-            average_loss = losses.avg
-    """
-    
     def __init__(self) -> None:
         self.avg = None
         self.sum = None
         self.count = None
     
-    def __init_counters(self, length: int) -> None:
-        """Initializes the `AverageMeter` counters.
-
-        Args:
-            length (int): number of elements to
-
-        Returns:
-            None:
-        """
+    def __init_counters(self, length):
         if length == 0:
             self.avg = 0
             self.sum = 0
@@ -42,16 +20,7 @@ class AverageMeter:
                 self.avg.append(0)
                 self.sum.append(0)
     
-    def update(self, val: Union[int, float, list], n: int) -> None:
-        """Updates `avg`.
-
-        Args:
-            val (Union[int, float, list]): value used to update `avg`.
-            n (int): identifies the number of times `val` must be repeated, e.g. how many batches had been used to define the `val`.
-
-        Returns:
-            None:
-        """
+    def update(self, val, n):
         if isinstance(val, list):
             if self.avg is None or self.sum is None or self.count is None:
                 self.__init_counters(len(val))
@@ -64,31 +33,49 @@ class AverageMeter:
             self.sum += val * n
             self.count += n
             self.avg = self.sum / self.count
+    
+    def value(self):
+        return self.avg
 
 
-class Accuracy:
+class RollingMeter:
+    def __init__(self) -> None:
+        self.outputs = torch.tensor([])
+        self.targets = torch.tensor([])
+    
+    def update(self, outputs, targets):
+        self.outputs = torch.cat((self.outputs, outputs), dim=0)
+        self.targets = torch.cat((self.targets, targets), dim=0)
+        return self.outputs, self.targets
+    
+    def __call__(self, *args, **kwargs):
+        return self.update(*args, **kwargs)
+    
+    def value(self):
+        raise NotImplementedError
+
+
+class Accuracy(RollingMeter):
     """Evaluates the predictions accuracy given an output `torch.Tensor` and a target `torch.Tensor`.
-
     Args:
         topk (Tuple[int, ...], optional): top-k accuracy identifiers. E.g. to evaluate both top-1 and top-5 accuracy `topk = (1, 5)`.
     """
     
-    def __init__(self, topk: Optional[Tuple[int, ...]] = (1,)) -> None:
+    def __init__(self, topk=(1,)):
+        super().__init__()
         self.topk = topk
     
-    def __call__(self, outputs: torch.Tensor, targets: torch.Tensor) -> list:
+    def value(self) -> list:
         """Evaluates the accuracy of the outputs given the targets.
-
-        Args:
-            outputs (torch.Tensor): tensor defining a prediction.
-            targets (torch.Tensor): tensor defining the targets.
-
         Returns:
             list: list of top-k accuracy, one for each element of `topk`.
-
         """
+        outputs, targets = self.outputs, self.targets
+        
         maxk = max(self.topk)
         batch_size = targets.shape[0]
+        if batch_size == 0:
+            raise RuntimeError('Warning: batch_size is 0')
         
         _, pred = outputs.topk(maxk, 1, True, True)
         pred = pred.t()
@@ -98,8 +85,17 @@ class Accuracy:
         
         for k in self.topk:
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
+            res.append(correct_k.mul_(1. / batch_size).item())
         
         if len(res) == 1:
             res = res[0]
+        
         return res
+
+
+def topk_accuracy(outputs, labels, topk=1):
+    outputs = torch.softmax(outputs, dim=1)
+    _, preds = outputs.topk(topk, dim=1)
+    preds = preds.t()
+    correct = preds.eq(labels.view(1, -1).expand_as(preds)).sum()
+    return (correct / float(len(outputs))).cpu().item()
