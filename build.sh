d8ee88ea26bcf50640f23ec39a3a913fa814b810
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-05-22 09:26:47 +0200

    update dockerfiles

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build -t eidos-service.di.unito.it/bragagnolo/zero-grad:latest . -f Dockerfile.python
-docker push eidos-service.di.unito.it/bragagnolo/zero-grad:latest
+docker build -t eidos-service.di.unito.it/bragagnolo/neq:latest . -f Dockerfile.python
+docker push eidos-service.di.unito.it/bragagnolo/neq:latest
 
-docker build -t eidos-service.di.unito.it/bragagnolo/zero-grad:sweep . -f Dockerfile.sweep
-docker push eidos-service.di.unito.it/bragagnolo/zero-grad:sweep
+docker build -t eidos-service.di.unito.it/bragagnolo/neq:sweep . -f Dockerfile.sweep
+docker push eidos-service.di.unito.it/bragagnolo/neq:sweep
