f07049a726e7437206508a50aa2a76577fed4546
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-06-09 10:43:01 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ docker push eidos-service.di.unito.it/bragagnolo/neq:base
 
 docker build -t eidos-service.di.unito.it/bragagnolo/neq:python . -f Dockerfile.python
 docker push eidos-service.di.unito.it/bragagnolo/neq:python
+
+docker build -t eidos-service.di.unito.it/bragagnolo/neq:sweep . -f Dockerfile.sweep
+docker push eidos-service.di.unito.it/bragagnolo/neq:sweep
