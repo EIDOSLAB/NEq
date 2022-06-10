bd558ddba88c97be0cb9c3790095e6637900db43
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-06-10 11:02:34 +0200

    windows build script

---
+++ build.ps1
@@ -0,0 +1,8 @@
+docker build -t eidos-service.di.unito.it/bragagnolo/neq:base . -f Dockerfile
+docker push eidos-service.di.unito.it/bragagnolo/neq:base
+
+docker build -t eidos-service.di.unito.it/bragagnolo/neq:python . -f Dockerfile.python
+docker push eidos-service.di.unito.it/bragagnolo/neq:python
+
+docker build -t eidos-service.di.unito.it/bragagnolo/neq:sweep . -f Dockerfile.sweep
+docker push eidos-service.di.unito.it/bragagnolo/neq:sweep
