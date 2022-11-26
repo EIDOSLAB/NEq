85426bd0e306a70007ecb9d6d0895a7cdd3a262e
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-11-26 11:09:32 +0100

    update

---
+++ src/Swin-Transformer/optim/__init__.py
@@ -0,0 +1,7 @@
+#  Copyright (c) 2021 EIDOSLab. All rights reserved.
+#  This file is part of the EIDOSearch library.
+#  See the LICENSE file for licensing terms (BSD-style).
+
+from .adam import MaskedAdam
+from .sgd import MaskedSGD
+from .adamw import MaskedAdamW
